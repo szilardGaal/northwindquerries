f856cc3c998da39d692721ef42b2ca3f25f0473c
szilardGaal <gaalszilard23@gmail.com>
2019-04-04 11:36:03 +0200

    fixed taks 4. hopefully

--- 4.sql
+++ 4.sql
@@ -1,5 +1,5 @@
-SELECT company_name, ARRAY_AGG(order_id) FROM customers
-INNER JOIN orders
+SELECT company_name, ARRAY_AGG(orders.order_id) FROM customers
+LEFT JOIN orders
 ON customers.customer_id = orders.customer_id
 GROUP BY company_name
 ORDER BY company_name ASC;
